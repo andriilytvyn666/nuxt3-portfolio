4b2300f9b5841f0df466f64dc4b230bc869b7e16
Andrii Lytvyn <lytvyn349@gmail.com>
2023-01-15 15:09:27 +0200

    feat: add courses, edu & workpaces sections

---
+++ components/Section/Courses.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="flex flex-col gap-4">
+        <span class="custom-section-title text-center">{{ "Certificates" }}</span>
+        <div id="skills-container"
+            class="grid grid-cols-2 gap-x-5 gap-y-5">
+            <ContainerCard v-for="i in 5" />
+        </div>
+    </div>
+</template>
