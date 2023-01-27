30b78636c2796462e1cb5cbc8143933c1a8b7e59
Andrii Lytvyn <lytvyn349@gmail.com>
2023-01-27 23:40:46 +0200

    refactor: remove console.log

--- nuxt/utils/utils.ts
+++ nuxt/utils/utils.ts
@@ -2,6 +2,5 @@ export const getLocalizedString = (
   locale: string,
   string: localeString
 ): string => {
-  console.log(locale === 'en' ? string.en : string.uk)
   return locale === 'en' ? string.en : string.uk
 }
