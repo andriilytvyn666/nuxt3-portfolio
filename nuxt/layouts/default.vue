eff27ffb274190b3aefb7868570cc5bc9fef3808
Andrii Lytvyn <lytvyn349@gmail.com>
2023-01-30 03:26:41 +0200

    feat(nuxt): add placeholder page and show the message "not ready" on mobile

--- nuxt/layouts/default.vue
+++ nuxt/layouts/default.vue
@@ -1,5 +1,28 @@
 <template>
-  <div class="flex flex-col items-stretch flex-grow min-h-screen">
+  <div
+    class="h-screen min-[0px]:max-[500px]:flex min-[500px]:max-2xl:hidden 2xl:hidden items-center justify-center p-16 gap-4 flex-col"
+  >
+    <span class="text-light text-xl font-medium">(｡╯︵╰｡)<br /></span>
+    <!-- <nuxt-icon class="text-light text-4xl" name="feather/x" filled /> -->
+    <span class="text-light text-xl font-medium text-center">
+      Mobile version is not ready yet.
+    </span>
+    <div class="h-min">
+      <SanityFile :asset-id="getLocalizedFile($i18n.locale, about)">
+        <template #default="{ src }">
+          <ButtonText
+            :text="$t('landing.aboutMe.downloadCV')"
+            :link="src"
+            icon="feather/download"
+            name="downloadCV"
+          />
+        </template>
+      </SanityFile>
+    </div>
+  </div>
+  <div
+    class="flex flex-col items-stretch flex-grow min-h-screen min-[0px]:max-[499px]:hidden"
+  >
     <Header />
     <article
       class="grid grid-cols-12 gap-5 justify-center grow xl:w-[73.75rem] lg:w-[62rem] md:w-[46rem] sm:w-[38rem] min-[500px]:w-[29.25rem] min-[320px]:w-[20.5rem] mx-auto"
@@ -14,6 +37,12 @@
 </template>
 
 <script setup lang="ts">
+import { useLandingStore } from '~~/stores/landing'
+
+const store = useLandingStore()
+const about = computed(() => store.about)
+store.fetchAbout()
+
 const i18nHead = useLocaleHead({
   addSeoAttributes: true,
 })
